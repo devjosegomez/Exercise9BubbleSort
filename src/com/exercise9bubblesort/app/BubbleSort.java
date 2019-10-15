0984118b2c6e1718d9e0f7ce3be795ca903a6681
devjosegomez <devjosegomez@gmail.com>
2019-10-15 12:31:22 -0500

    Added BlubbleSort class

--- src/com/exercise9bubblesort/app/BubbleSort.java
+++ src/com/exercise9bubblesort/app/BubbleSort.java
@@ -1,9 +1,26 @@
 package com.exercise9bubblesort.app;
 
+import java.util.Random;
+
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		final int ELEMENTS_ARRAY = 100;
+		int tmp = 0;
+		int[] unorderedArray = new int[ELEMENTS_ARRAY];
+		int[] orderedArray = new int[ELEMENTS_ARRAY];
+		
+		Random randomNumbers = new Random(System.nanoTime());
+		
+		for(int i=0; i<ELEMENTS_ARRAY; i++) {
+			unorderedArray[i] = randomNumbers.nextInt(101);
+		}
+		
+		for(int i=0; i<ELEMENTS_ARRAY; i++) {
+			System.out.println(unorderedArray[i] + "");
+		}
+		
+		//method bubble
 
 	}
 
